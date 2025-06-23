1867ff84c432c1519dde5ba673c83f158f2b9358
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-23 08:41:02 +0300

    Testing mut And immutable Variables

--- From-Youtube-01/hello_rust/src/main.rs
+++ From-Youtube-01/hello_rust/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+
+    let y = 10;
+
+    // y = 20; // This Will Raise Compile Error
+
+    let mut x = 20;
+
+    println!("The Value Of X, Before Changing Is: {}", x);
+
+    x = 25; // If We Don't Use The Previous Value, Warning Will Appear
+
+    println!("Hello, world!, y is: {}, x is: {}, x+y is: {}", y, x, y+x);
+
+
 }
