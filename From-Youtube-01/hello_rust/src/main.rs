cbedef2eedc0014be121b383ac98850282b4ea90
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-06-23 09:02:41 +0300

    Testing Integers Types

--- From-Youtube-01/hello_rust/src/main.rs
+++ From-Youtube-01/hello_rust/src/main.rs
@@ -13,4 +13,14 @@ fn main() {
     println!("Hello, world!, y is: {}, x is: {}, x+y is: {}", y, x, y+x);
 
 
+    /*
+     * Testing The Variables Types
+     */
+    let a1 = 98_999;
+    let a2 = 0xfff;
+    let a3 = 0o337;
+    let a4 = 0b1111_0000;
+    let a5 = b'a';
+
+    println!("a1 is: {}, a2 is: {}, a3 is: {}, a4 is: {}, a5 is: {}", a1, a2, a3, a4, a5)
 }
