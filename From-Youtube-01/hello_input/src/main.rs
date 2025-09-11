57d814bbb8330300cc9ae61afd61cb57758e41d3
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-11 13:04:26 +0300

    Adding types to variables

--- From-Youtube-01/hello_input/src/main.rs
+++ From-Youtube-01/hello_input/src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() {
     println!("Please enter your age: ");
 
-    let mut input = String::new();
+    let mut input: String = String::new();
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    let input = input.trim();
+    let input: &str = input.trim();
 
     let input: u32 = input.parse().expect("Please enter a number!");
 
