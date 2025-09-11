751144fee411aa40a07d13aad028240c438f1286
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-11 13:51:57 +0300

    adding for-loop

---
+++ From-Youtube-01/for-loop/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+    for num in arr {
+        println!("The num is: {num}");
+    }
+
+    println!("----------------------------------");
+
+    for i in 1..6 {
+        println!("i is: {i}");
+    }
+
+    println!("----------------------------------");
+
+    println!("Hello, world!");
+}
