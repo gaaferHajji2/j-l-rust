b96f0b324622078d3255d8eab14dff1221a9cd0f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-08 13:24:13 +0300

    Using slice and mut array

--- From-Youtube-01/for-loop/src/main.rs
+++ From-Youtube-01/for-loop/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
 
+    // immutable array with values
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
 
     for num in arr {
@@ -17,5 +18,16 @@ fn main() {
     }
     println!("----------------------------------");
 
+    // get slice of array from 1 to 4 (Note: array start from 0)
+    let arr_slice = &arr[1..5];
+    println!("The array slice is: {:?}", arr_slice);
+    println!("----------------------------------");
+
     println!("Hello, world!");
+    println!("----------------------------------");
+
+    // mutable array with values
+    let mut arr2:[i32; 5] = [0, 1, 2, 3, 4];
+    arr2[0] = 500000;
+    println!("The array is: {:?}", arr2);
 }
