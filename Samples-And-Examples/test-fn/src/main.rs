2f1b605a3bb9c0d183e5f281dd0b4d2f89d7fb4c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-13 10:28:50 +0300

    Test lifetime with functions && ref

--- Samples-And-Examples/test-fn/src/main.rs
+++ Samples-And-Examples/test-fn/src/main.rs
@@ -7,6 +7,15 @@ fn mul(t1: f64, t2: f64) -> f64 {
     t1 * t2 // here if we don't set ; then it will be used as: return t1 * t2;
 }
 
+// test same lifetime for variables
+fn longest<'a> (t1: &'a str, t2: &'a str) -> &'a str {
+    if t1.len() > t2.len() {
+        t1
+    } else {
+        t2
+    }
+}
+
 fn main() {
     println!("The add is: {}", add(1, 2));
     println!("The mul is: {}", mul(2.1, 3.2));
@@ -14,5 +23,11 @@ fn main() {
 
     let t1: String = String::from("Hello Jafar Loka String Function");
 
-    println!("The msg is: {}", t1)
+    println!("The msg is: {}", t1);
+
+    let t2: String = String::from("Jafar-Loka-01");
+    let t3: String = String::from("Jafar-Loka-02-ITE Developer");
+
+    let result: &str = longest(&t2, &t3);
+    println!("The longest string is: {}", result)
 }
