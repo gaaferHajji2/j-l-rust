171c1e7253a3c639c3229b04c722d0781d1a5ec6
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-13 10:49:59 +0300

    Test cargo doc

---
+++ Samples-And-Examples/test-doc/src/main.rs
@@ -0,0 +1,15 @@
+
+/// Add Function: Adds two int values
+/// 
+/// # Arguments
+/// * 'a' the first value
+/// * 'b' the second value
+/// 
+/// # Returns: the sum of a + b
+fn add(a: i32, b:i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    println!("Hello, world!");
+}
