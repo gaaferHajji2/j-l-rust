7c9d3f18cd02b801681c9fe6d9da22bdcdee4cc3
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-22 09:41:41 +0300

    Using if-let with Result && Ok

--- Samples-And-Examples/if-let/src/main.rs
+++ Samples-And-Examples/if-let/src/main.rs
@@ -6,5 +6,13 @@ fn main() {
         println!("t2 is...")
     }
 
+    let t3: Result<i32, &str> = Ok(1);
+
+    if let Ok(t4) = t3 {
+        println!("The value of t4 is: {}", t4);
+    } else {
+        println!("The value is ....")
+    }
+
     println!("Hello, world!");
 }
