f1a98c244b0e079a7febac312d4f4af1d8164625
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-05 11:38:37 +0300

    check the response status and text

--- Rust-async-and-concurrent/simple-request/src/main.rs
+++ Rust-async-and-concurrent/simple-request/src/main.rs
@@ -6,19 +6,26 @@ async fn main() -> Result<(), Error> {
     let url = "https://jsonplaceholder.typicode.com/todos/1";
     let start_time = Instant::now();
 
-    let _ = reqwest::get(url).await?;
+    let response = reqwest::get(url).await?;
     let total_time = start_time.elapsed();
     println!("total time is: {}", total_time.as_millis());
 
-    let start_time = Instant::now();
-    let (_, _, _, _) = tokio::join!(
-        reqwest::get(url),
-        reqwest::get(url),
-        reqwest::get(url),
-        reqwest::get(url),
-    );
-    let total_time = start_time.elapsed();
-    println!("total time is: {}", total_time.as_millis());
+    // let start_time = Instant::now();
+    // let (_, _, _, _) = tokio::join!(
+    //     reqwest::get(url),
+    //     reqwest::get(url),
+    //     reqwest::get(url),
+    //     reqwest::get(url),
+    // );
+    // let total_time = start_time.elapsed();
+    // println!("total time is: {}", total_time.as_millis());
+
+    if response.status().is_success() {
+        let t1 = response.text().await?;
+        println!("{}", t1)
+    } else {
+        println!("Response not ok with status: {}", response.status())
+    }
 
     Ok(())
 }
