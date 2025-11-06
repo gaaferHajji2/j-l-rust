aae9032e01e0f5ebea99f6fe5311fb80ed2c6b88
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-06 10:19:49 +0300

    Finish from traits with state Map

--- Rust-actix-web-examples-and-cli/simple_todo/src/to_do/enums.rs
+++ Rust-actix-web-examples-and-cli/simple_todo/src/to_do/enums.rs
@@ -5,14 +5,14 @@ pub enum TaskStatus {
     PENDING
 }
 
-// impl TaskStatus {
-//     pub fn stringify(&self) -> String {
-//         match &self {
-//             &Self::DONE => {"DONE".to_string()},
-//             &Self::PENDING => {"PENDING".to_string()}
-//         }
-//     }
-// }
+impl TaskStatus {
+    pub fn stringify(&self) -> String {
+        match &self {
+            &Self::DONE => {"DONE".to_string()},
+            &Self::PENDING => {"PENDING".to_string()}
+        }
+    }
+}
 
 impl fmt::Display for TaskStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
--- Rust-actix-web-examples-and-cli/simple_todo/src/to_do/traits/get.rs
+++ Rust-actix-web-examples-and-cli/simple_todo/src/to_do/traits/get.rs
@@ -1,5 +1,15 @@
+use serde_json::Map;
+use serde_json::value::Value;
+
 pub trait Get {
-    fn get(&self, title: &str) {
-        println!("Get todo with title: {}", title)
+    fn get(&self, title: &String, state: &Map<String, Value>) {
+        let item: Option<&Value> = state.get(title);
+        match item {
+            Some(value) => {
+                println!("The title is: {}", title);
+                println!("the status is: {}\n", value);
+            }
+            None => println!("Todo with title {} not found", title)
+        }
     }
 }
