e1033911fc9934503dc8232bd9aadb56c12a8c62
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-12 12:46:30 +0300

    Configure views factory

--- Rust-actix-web-examples-and-cli/simple_web_app/src/main.rs
+++ Rust-actix-web-examples-and-cli/simple_web_app/src/main.rs
@@ -11,6 +11,7 @@ async fn say_hello(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+        .configure(views::views_factory)
         .route("/", web::get().to(say_hello))
         .route("/{name}", web::get().to(say_hello))
         .route("/hello/jloka", web::get().to(|| async { "Hello JLoka World"}))
