9ce729cb867921b3c69177f6b7e0dc4e5b713b26
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-20 13:18:30 +0300

    Remove unused imports & implement get

--- Rust-actix-web-examples-and-cli/simple_web_app/src/views/todo/get.rs
+++ Rust-actix-web-examples-and-cli/simple_web_app/src/views/todo/get.rs
@@ -1,23 +1,6 @@
-use actix_web::{Responder, web};
-use serde_json::value::Value;
-use serde_json::Map;
-
-use crate::state::read_file;
-use crate::to_do::{ItemTypes, to_do_factory, enums::TaskStatus};
+use actix_web::{Responder};
 use crate::json_serialization::to_do_items::ToDoItems;
 
 pub async fn get() -> impl Responder {
-    let state: Map<String, Value> = read_file("./jloka.json");
-
-    let mut t1: Vec<ItemTypes> = Vec::new();
-
-    for (key, value) in state {
-         let status: TaskStatus = TaskStatus::from_string(value.as_str().unwrap().to_string());
-        let item: ItemTypes = to_do_factory(&key, status);
-        t1.push(item);
-    }
-
-    let t2: ToDoItems = ToDoItems::new(t1);
-
-    web::Json(t2)
+    return ToDoItems::get_state();
 }
