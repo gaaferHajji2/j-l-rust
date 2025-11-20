180fcbc7c5f3744f8f04ab7a63c903bac5a48e5a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-20 13:13:06 +0300

    Implement responder for todo items

--- Rust-actix-web-examples-and-cli/simple_web_app/src/json_serialization/to_do_items.rs
+++ Rust-actix-web-examples-and-cli/simple_web_app/src/json_serialization/to_do_items.rs
@@ -1,6 +1,12 @@
-use crate::to_do::ItemTypes;
+use crate::state::{read_file};
+use crate::to_do::{ItemTypes, to_do_factory};
+use crate::to_do::enums::TaskStatus;
 use crate::to_do::structs::base::Base;
+use actix_web::http::header::ContentType;
+use actix_web::{Responder, HttpRequest, HttpResponse};
+use actix_web::body::BoxBody;
 use serde::Serialize;
+use serde_json::{Map, value::Value};
 
 #[derive(Serialize)]
 pub struct ToDoItems {
@@ -31,5 +37,28 @@ impl ToDoItems {
             pending_item_count: pending_count,
             done_item_count: done_count,
         };
+    }
+
+    pub fn get_state() -> ToDoItems {
+        let state: Map<String, Value> = read_file("./jloka.json");
+        let mut t1: Vec<_> = Vec::new();
+
+        for (key, value) in state {
+            let status = TaskStatus::from_string(value.as_str().unwrap().to_string());
+            let t2 = to_do_factory(&key, status);
+            t1.push(t2);
+        }
+
+        return ToDoItems::new(t1);
+        
+    }
+}
+
+impl Responder for ToDoItems {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let t1 = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok().content_type(ContentType::json()).body(t1)
     }
 }
