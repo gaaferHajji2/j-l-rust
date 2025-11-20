05415f5652b8495a9f0da1d6a01e7da4981111e1
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-20 12:46:30 +0300

    Implement get to do items

--- Rust-actix-web-examples-and-cli/simple_web_app/src/json_serialization/to_do_items.rs
+++ Rust-actix-web-examples-and-cli/simple_web_app/src/json_serialization/to_do_items.rs
@@ -1,17 +1,35 @@
-use serde::Serialize;
 use crate::to_do::ItemTypes;
 use crate::to_do::structs::base::Base;
+use serde::Serialize;
 
 #[derive(Serialize)]
- pub struct ToDoItems {
+pub struct ToDoItems {
     pub pending_items: Vec<Base>,
     pub done_items: Vec<Base>,
     pub pending_item_count: i8,
-    pub done_item_count: i8
+    pub done_item_count: i8,
 }
 
 impl ToDoItems {
     pub fn new(input_items: Vec<ItemTypes>) -> ToDoItems {
-        
+        let mut pending_array: Vec<Base> = Vec::new();
+        let mut done_array: Vec<Base> = Vec::new();
+
+        for item in input_items {
+            match item {
+                ItemTypes::Pending(t1) => pending_array.push(t1.super_base),
+                ItemTypes::Done(t1) => done_array.push(t1.super_struct),
+            }
+        }
+
+        let done_count: i8 = done_array.len() as i8;
+        let pending_count: i8 = pending_array.len() as i8;
+
+        return ToDoItems {
+            pending_items: pending_array,
+            done_items: done_array,
+            pending_item_count: pending_count,
+            done_item_count: done_count,
+        };
     }
 }
--- Rust-actix-web-examples-and-cli/simple_web_app/src/views/todo/get.rs
+++ Rust-actix-web-examples-and-cli/simple_web_app/src/views/todo/get.rs
@@ -3,9 +3,21 @@ use serde_json::value::Value;
 use serde_json::Map;
 
 use crate::state::read_file;
+use crate::to_do::{ItemTypes, to_do_factory, enums::TaskStatus};
+use crate::json_serialization::to_do_items::ToDoItems;
 
 pub async fn get() -> impl Responder {
     let state: Map<String, Value> = read_file("./jloka.json");
 
-    web::Json(state)
+    let mut t1: Vec<ItemTypes> = Vec::new();
+
+    for (key, value) in state {
+         let status: TaskStatus = TaskStatus::from_string(value.to_string());
+        let item: ItemTypes = to_do_factory(&key, status);
+        t1.push(item);
+    }
+
+    let t2: ToDoItems = ToDoItems::new(t1);
+
+    web::Json(t2)
 }
