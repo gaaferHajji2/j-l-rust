241f2f7237fd94c64b008014340e48af9c12ca46
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 05:38:56 +0300

    Fix imports & build struct for query data & build new method for query params

--- test-01/api-project/src/main.rs
+++ test-01/api-project/src/main.rs
@@ -1,9 +1,16 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web::{Query, Path}};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct JLoka {
+    name: String,
+    age: i32
+}
 
 #[actix_web::main]
 async fn main() {
     HttpServer::new(|| {
-        App::new().service(return_hello).service(get_name)
+        App::new().service(return_hello).service(get_name).service(get_query_data)
     })
     .bind("0.0.0.0:3000")
     .unwrap()
@@ -18,7 +25,13 @@ async fn return_hello() -> impl Responder {
 }
 
 #[get("/name/{name}")]
-async fn get_name(name: web::Path<String>) -> impl Responder {
+async fn get_name(name: Path<String>) -> impl Responder {
     let msg: String = format!("The name is: {}", name.into_inner());
     return HttpResponse::Ok().body(msg)
+}
+
+#[get("/query")]
+async fn get_query_data(query_data: Query<JLoka>) -> impl  Responder {
+    let jloka_msg = format!("The name is: {}, the age is: {}", query_data.name, query_data.age);
+    return HttpResponse::Ok().body(jloka_msg);
 }
