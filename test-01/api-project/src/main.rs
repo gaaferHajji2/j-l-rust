1d7218599b62b1dda0656dae103aff4d1b4205f7
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 05:46:22 +0300

    Build method for json request body

--- test-01/api-project/src/main.rs
+++ test-01/api-project/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web::{Query, Path}};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web::{Json, Path, Query}};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -10,7 +10,11 @@ struct JLoka {
 #[actix_web::main]
 async fn main() {
     HttpServer::new(|| {
-        App::new().service(return_hello).service(get_name).service(get_query_data)
+        App::new()
+            .service(return_hello)
+            .service(get_name)
+            .service(get_query_data)
+            .service(get_jloka_data)
     })
     .bind("0.0.0.0:3000")
     .unwrap()
@@ -34,4 +38,9 @@ async fn get_name(name: Path<String>) -> impl Responder {
 async fn get_query_data(query_data: Query<JLoka>) -> impl  Responder {
     let jloka_msg = format!("The name is: {}, the age is: {}", query_data.name, query_data.age);
     return HttpResponse::Ok().body(jloka_msg);
+}
+#[post("/jloka")]
+async fn get_jloka_data(jloka_data: Json<JLoka>) -> impl Responder {
+    let data = format!("The username is: {} & age is: {}", jloka_data.name, jloka_data.age);
+    return HttpResponse::Ok().body(data);
 }
