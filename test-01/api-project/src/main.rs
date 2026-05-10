2fb58ac2d4485c122bbc758b825053483395993a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 05:16:45 +0300

    Build the server using the service & factory

--- test-01/api-project/src/main.rs
+++ test-01/api-project/src/main.rs
@@ -1,13 +1,18 @@
-use actix_web::{App, HttpResponse, HttpServer, web::get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
 #[actix_web::main]
 async fn main() {
     HttpServer::new(|| {
-        App::new().route("/", get().to(|| async { HttpResponse::Ok().body("get")}))
+        App::new().service(return_hello)
     })
     .bind("0.0.0.0:3000")
     .unwrap()
     .run()
     .await
     .unwrap();
+}
+
+#[get("/")]
+async fn return_hello() -> impl Responder {
+    return HttpResponse::Ok().body("Jafar Loka-01");
 }
