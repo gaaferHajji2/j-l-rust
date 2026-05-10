258ab6454d49dc3d2a2dd0be086fb3eb3407723d
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-10 05:08:30 +0300

    Build actix server with get method

--- test-01/api-project/src/main.rs
+++ test-01/api-project/src/main.rs
@@ -1,4 +1,13 @@
+use actix_web::{App, HttpResponse, HttpServer, web::get};
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| {
+        App::new().route("/", get().to(|| async { HttpResponse::Ok().body("get")}))
+    })
+    .bind("0.0.0.0:3000")
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
 }
