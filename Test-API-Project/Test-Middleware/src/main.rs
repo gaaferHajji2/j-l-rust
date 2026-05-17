3851b322891e76e8b201f7fb6ea9b9bf587c585e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-17 05:58:46 +0300

    Testing route scope with middlewares

--- Test-API-Project/Test-Middleware/src/main.rs
+++ Test-API-Project/Test-Middleware/src/main.rs
@@ -1,9 +1,16 @@
-use actix_web::{App, Error, HttpResponse, HttpServer, Responder, body::MessageBody, dev::{ServiceRequest, ServiceResponse}, get, middleware::{Next, from_fn}};
+use actix_web::{App, Error, HttpResponse, HttpServer, Responder, body::MessageBody, dev::{ServiceRequest, ServiceResponse}, get, middleware::{Next, from_fn}, web};
 
 #[actix_web::main]
 async fn main() {
     HttpServer::new(|| {
-        App::new().service(jloka_hello).service(jloka_world).wrap(from_fn(my_middleware))
+        App::new()
+            .service(jloka_hello)
+            .service(
+                web::scope("/world")
+                    .route("/world", web::get().to(jloka_world))
+                    .wrap(from_fn(my_middleware))
+            )
+            // .wrap(from_fn(my_middleware))
     }).bind("0.0.0.0:3000").unwrap().run().await.unwrap()
 }
 
@@ -12,13 +19,13 @@ async fn jloka_hello() -> impl Responder {
     return HttpResponse::Ok().body("Hello World from Jafar Loka-01")
 }
 
-#[get("/world")]
+// #[get("/world")]
 async fn jloka_world() -> impl Responder {
     return HttpResponse::Ok().body("Hello World from Jafar Loka-02")
 }
 
 async fn my_middleware(req: ServiceRequest, next: Next<impl MessageBody>) -> Result<ServiceResponse<impl MessageBody>, Error> {
     println!("Hello From My Middleware");
-    Ok(req.into_response(HttpResponse::Unauthorized().body("Unauthorized Test")))
-    // next.call(req).await
+    // Ok(req.into_response(HttpResponse::Unauthorized().body("Unauthorized Test")))
+    next.call(req).await
 }
