d205466906a12d9ff6402b705003d21094331035
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-18 05:59:02 +0300

    Adding the guard to get method only

--- Test-API-Project/test-guard/src/main.rs
+++ Test-API-Project/test-guard/src/main.rs
@@ -1,5 +1,21 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, guard, web};
 
 #[actix_web::main]
-fn main() {
-    
+async fn main() {
+    HttpServer::new( move || {
+        App::new().service(
+            web::scope("/api")
+            .guard(guard::Get())
+            .route("/hello", web::get().to(hello))
+            .route("/world", web::post().to(world))
+        )
+    }).bind("0.0.0.0:3000").unwrap().run().await.unwrap()
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("JLoka From Hello Path")
+}
+
+async fn world() -> impl Responder {
+    HttpResponse::Ok().body("JLoka From World Path")
 }
